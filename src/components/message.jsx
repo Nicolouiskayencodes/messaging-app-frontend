b089cc6237f83c681d88f105383ab0c84bc91848
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-12-13 22:57:57 -0800

    Display images instead of imageurl

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function Message({message, user}) {
   return(<>
-    {message.authorId === user.id ? (<div className="sent"><p>{message.content}{message.image}</p><p>{message.author.displayName || message.author.username}</p><p>{Date(message.createdA)}</p></div>): (<div className="received"><p>{message.content}</p><p>{message.author.displayName || message.author.username}</p><p>{Date(message.createdA)}</p></div>)}
+    {message.authorId === user.id ? (<div className="sent"><img src={message.image}/> <p>{message.content}</p><p>{message.author.displayName || message.author.username}</p><p>{Date(message.createdA)}</p></div>): (<div className="received"><img src={message.image}/><p>{message.content}</p><p>{message.author.displayName || message.author.username}</p><p>{Date(message.createdA)}</p></div>)}
   </>)
 }
 
